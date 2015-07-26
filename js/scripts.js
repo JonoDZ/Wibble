a936dab5cfcde327838b79fc73901f25182fdfa3
Jono <jono@oclocaladmins-MacBook-Pro.local>
2015-07-26 19:47:00 +0100

    something broke, not sure what, this commit was previously working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 
-  var imageChildren = document.getElementById("list").childElementCount;
+var imageCount = document.getElementById("list").childElementCount;
 
 
 var mouseAlignEl = 0;
@@ -19,19 +19,28 @@ function getMouseXY(e) {
     mouseX = e.pageX
     mouseY = e.pageY
   
-
+    //set target image to move in relation to the mouse
     document.getElementById("list").children[15].style.marginLeft = String(mouseX) + "px";
     
+    //simulates target child
     mouseAlignEl = 15;
+    var jConst = mouseAlignEl;
+    var jUp = mouseAlignEl + 10;
 
+    //adjust alignment of images below the Target image
     for (var i = 1; i < 9; i++) {
       document.getElementById("list").children[mouseAlignEl + i].style.marginLeft = String(mouseX - i) + "px";
     }
-
+    //adjust alignment of images above the Target image
     for (var i = 8; i > 0; i--) {
       document.getElementById("list").children[mouseAlignEl - i].style.marginLeft = String(mouseX - i) + "px";
     }
 
+    var lastMargin = document.getElementById("list").children[mouseAlignEl+10].style.marginLeft;
+
+    for (jUp; jUp < imageCount; jUp++){
+      document.getElementById("list").children[jUp].style.marginLeft = "100px"; 
+    }
 
   if (mouseY < 0){mouseY = 0}  
 
