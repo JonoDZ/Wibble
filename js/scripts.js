4045e5c8f474f112e962b6ac46cdc8d269bd420f
Jono <jono@oclocaladmins-MacBook-Pro.local>
2015-07-26 22:04:44 +0100

    X and Y motions work; more bugs than worth listing at this point. Core functionality coming together.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 
 
 var imageCount = document.getElementById("list").childElementCount;
-
-
-var mouseAlignEl = 0;
+var listCss = document.getElementById("list");
+var listMargin = document.defaultView.getComputedStyle(listCss, null).getPropertyValue('margin-left');
+var imageHeight = 1;
+var targetEl = 0;
+var baseMovement = .1;
+var baseMultiplier = 1;
+var NumOfImgsToRipple = 40;
 
 // Set-up to use getMouseXY function onMouseMove
 document.onmousemove = getMouseXY;
@@ -12,41 +16,52 @@ document.onmousemove = getMouseXY;
 var mouseX = 0
 var mouseY = 0
 
+
 // Main function to retrieve mouse x-y pos.s
-
 function getMouseXY(e) {
-   // grab the x-y pos
-    mouseX = e.pageX
-    mouseY = e.pageY
-  
-    //set target image to move in relation to the mouse
-    document.getElementById("list").children[15].style.marginLeft = String(mouseX) + "px";
-    
-    //simulates target child
-    mouseAlignEl = 15;
-    var jConst = mouseAlignEl;
-    var jUp = mouseAlignEl + 10;
 
-    //adjust alignment of images below the Target image
-    for (var i = 1; i < 9; i++) {
-      document.getElementById("list").children[mouseAlignEl + i].style.marginLeft = String(mouseX - i) + "px";
-    }
-    //adjust alignment of images above the Target image
-    for (var i = 8; i > 0; i--) {
-      document.getElementById("list").children[mouseAlignEl - i].style.marginLeft = String(mouseX - i) + "px";
-    }
+    //negitive value capture
+    if (mouseY < 0){mouseY = 0}  
+    if (mouseX < 0){mouseX = 0}
 
-    var lastMargin = document.getElementById("list").children[mouseAlignEl+10].style.marginLeft;
+    //get mouse X position, create a 'white space' between mouse and area of effect
+    mouseX = e.pageX - parseInt(listMargin) + 10;
+    //get mouse Y position
+    mouseY = e.pageY;
+   
+    //Calculate the X/Y axis motion of the images
+    moveImages(mouseX, Math.round(mouseY / imageHeight));
 
-    for (jUp; jUp < imageCount; jUp++){
-      document.getElementById("list").children[jUp].style.marginLeft = "100px"; 
-    }
-
-  if (mouseY < 0){mouseY = 0}  
-
- 
   return true
 
 
 }
 
+function moveImages (mouseXpos, imageToMove) {
+
+      //set target image to move in relation to the mouse
+    document.getElementById("list").children[imageToMove].style.marginLeft = String(mouseXpos) + "px";
+    
+    //simulates target child
+    var ImagesAfterRipple = imageToMove + NumOfImgsToRipple + 1;
+
+    //adjust alignment of images below the Target image
+    for (var i = 0; i < NumOfImgsToRipple; i++) {
+
+      var amountToMove = String(mouseXpos - Math.pow(i * baseMovement, 2)) + "px";
+      document.getElementById("list").children[imageToMove + i].style.marginLeft = amountToMove;
+    }
+    //adjust alignment of images above the Target image
+    for (var i = NumOfImgsToRipple; i > 0; i--) {
+
+      var amountToMove = String(mouseXpos - Math.pow(i * baseMovement, 2)) + "px";
+      document.getElementById("list").children[imageToMove - i].style.marginLeft = amountToMove;
+    }
+
+    var lastRippleMargin = document.getElementById("list").children[imageToMove + NumOfImgsToRipple].style.marginLeft;
+
+    for (ImagesAfterRipple; ImagesAfterRipple < (imageCount - 1); ImagesAfterRipple++){
+      document.getElementById("list").children[ImagesAfterRipple].style.marginLeft = lastRippleMargin; 
+    }
+
+}
